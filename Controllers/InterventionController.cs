c103726877bf1503d30ed3c6e684541576327314
saadeddine <saadeddine.feki@gmail.com>
2020-11-20 10:54:20 -0500

    David suggestion

--- Controllers/InterventionController.cs
+++ Controllers/InterventionController.cs
@@ -49,19 +49,19 @@ namespace RocketApi.Controllers
         //** /api/Intervention/1/completed
         // /api/Intervention/1/completed
         [HttpPut("{id}/{status}")]
-        public async Task<IActionResult> CompletedInterventions([FromRoute] long id, [FromRoute] string status, Interventions intervention)
+        public async Task<IActionResult> CompletedInterventions([FromRoute] long id, [FromRoute] string status)
         {
             // verify the status from the route and handle the response
             if (status == "inprogress")
             {
                 var update = await _context.Interventions.FindAsync(id);
                 // verify if the id in the route is the same in the Body
-                if (update == null || id != intervention.Id)
+                if (update == null)
                 {
                     return Content("Wrong id ! please check and try again");
                 }
                 // verify the spelling 
-                if (intervention.Status == "InProgress")
+                if (status == "InProgress")
                 {
                     // update date and status
                     update.StartIntervention = DateTime.Now;
@@ -77,36 +77,11 @@ namespace RocketApi.Controllers
                 _context.Interventions.Update(update);
                 await _context.SaveChangesAsync();
                 // confirmation message
-                return Content("Intervention: " + intervention.Id + ", status has been changed to: " + intervention.Status);
+                return Content("Intervention: " + update.Id + ", status has been changed to: " + update.Status);
             }
             // 2 case completed
-            else if (status == "completed")
-            {
-                // check the id
-                var update = await _context.Interventions.FindAsync(id);
-                if (update == null || id != intervention.Id)
-                {
-                    return Content("Wrong id ! please check and try again");
-                }
-                // check the sppelling to prevent and standardize data
-                if (intervention.Status == "Completed")
-                {
-                    update.EndIntervention = DateTime.Now;
-                    update.Status = "Completed";
-                }
 
-                else
-                {
-                    // help the user to understand whats wrong !
-                    return Content("Please insert a valid status : Completed. Tray again please !  ");
-                }
-                // update and save
-                _context.Interventions.Update(update);
-                await _context.SaveChangesAsync();
 
-                return Content("Intervention: " + intervention.Id + ", status has been changed to: " + intervention.Status);
-
-            }
 
             // help the user to specify the endpoints
             else return Content("hem hem .. something wrong! please use this route format: [api/Intervention/{id}/inprogress] or  [api/Intervention/{id}/completed] and change the status in the body of the request");
