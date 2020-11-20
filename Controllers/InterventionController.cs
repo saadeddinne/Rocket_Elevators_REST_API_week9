a9a18975e79fa878a5a6fd2076be912b7df6b0c7
saadeddine <saadeddine.feki@gmail.com>
2020-11-20 08:25:42 -0500

    Postman collection

--- Controllers/InterventionController.cs
+++ Controllers/InterventionController.cs
@@ -25,12 +25,8 @@ namespace RocketApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Interventions>>> GetInterventions()
         {
-            var list = await _context.Interventions.Where(intervention => intervention.StartIntervention == null && intervention.Status == "Pending").ToListAsync();
-            // if (list is null)
-            // {
-            return list;
-            // }
-            // else return Content("No records that do not have a start date and are in Pending status.");
+            return await _context.Interventions.Where(intervention => intervention.StartIntervention == null && intervention.Status == "Pending").ToListAsync();
+
         }
 
         // GET: Returns a specific intervention by id
