c30aa6245a3b036939f62548e43ed2797f01d7b1
saadeddine <saadeddine.feki@gmail.com>
2020-12-03 01:15:45 -0500

    first Get: customer by email

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -32,18 +32,18 @@ namespace RocketApi.Controllers
         }
 
         // GET: api/Customers/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Customers>> GetCustomers(long id)
-        {
-            var customers = await _context.Customers.FindAsync(id);
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<Customers>> GetCustomers(long id)
+        // {
+        //     var customers = await _context.Customers.FindAsync(id);
 
-            if (customers == null)
-            {
-                return NotFound();
-            }
+        //     if (customers == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            return customers;
-        }
+        //     return customers;
+        // }
 
 
         // Get customer by email
