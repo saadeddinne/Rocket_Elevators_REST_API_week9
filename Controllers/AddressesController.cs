edd8d5d623a376f26427134227eb7d6614885d66
saadeddine <saadeddine.feki@gmail.com>
2020-12-15 09:13:08 -0500

    adding Addresses

---
+++ Controllers/AddressesController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RocketApi.Models;
+
+namespace RocketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly RocketContext _context;
+
+        public AddressesController(RocketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Addresses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Addresses>>> GetAddresses()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+
+        // GET: api/Addresses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Addresses>> GetAddresses(long id)
+        {
+            var addresses = await _context.Addresses.FindAsync(id);
+
+            if (addresses == null)
+            {
+                return NotFound();
+            }
+
+            return addresses;
+        }
+
+        // PUT: api/Addresses/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddresses(long id, Addresses addresses)
+        {
+            if (id != addresses.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(addresses).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Addresses
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Addresses>> PostAddresses(Addresses addresses)
+        {
+            _context.Addresses.Add(addresses);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAddresses", new { id = addresses.Id }, addresses);
+        }
+
+        // DELETE: api/Addresses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Addresses>> DeleteAddresses(long id)
+        {
+            var addresses = await _context.Addresses.FindAsync(id);
+            if (addresses == null)
+            {
+                return NotFound();
+            }
+
+            _context.Addresses.Remove(addresses);
+            await _context.SaveChangesAsync();
+
+            return addresses;
+        }
+
+        private bool AddressesExists(long id)
+        {
+            return _context.Addresses.Any(e => e.Id == id);
+        }
+    }
+}
